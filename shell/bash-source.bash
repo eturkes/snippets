c3801bd403880fab6a1d60eb120f0fc2d9d4ac1b
Emir Turkes <eturkes@bu.edu>
2018-01-04 07:39:17 +0900

    Add new snippets

---
+++ shell/bash-source.bash
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+# Get the location of this script regardless of where it is called from
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" > /dev/null && pwd )"
---
+++ shell/experiment-to-binaries.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# Move all Intellicage .experiment files to binaries folder
+# To be used in top-level of Intellicage Archive
+
+for d in */ ; do
+    ARCHIVE_EXPERIMENTS=${PWD}/"${d}"Experiments
+    BINARIES_EXPERIMENTS=../../binaries/"${PWD#${PWD%/*/*}/}"/"${d}"Experiments
+    
+    ls "${ARCHIVE_EXPERIMENTS}"
+    
+    mkdir -p "${BINARIES_EXPERIMENTS}" 
+    mv "${ARCHIVE_EXPERIMENTS}"/*experiment "${BINARIES_EXPERIMENTS}"
+    ln -sr "${BINARIES_EXPERIMENTS}"/*experiment "${ARCHIVE_EXPERIMENTS}"
+done
