0b3d278600f38cd8dfebbbcb704d51dea8ef0561
Emir Turkes <eturkes@bu.edu>
2018-01-05 17:46:52 +0900

    Add attribution

--- shell/bash-source.bash
+++ shell/bash-source.bash
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 
+# Implementation derived from Stack Overflow
+# https://stackoverflow.com/questions/59895/getting-the-source-directory-of-a-bash-script-from-within
+# Question asked by: Jiaaro https://stackoverflow.com/users/2908/jiaaro
+# Answer given by: Dave Dopson https://stackoverflow.com/users/407731/dave-dopson
+
 # Get the location of this script regardless of where it is called from
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" > /dev/null && pwd )"
