d6ad6425763f5c05802791adbae9418cd9d52331
Emir Turkes <eturkes@bu.edu>
2018-02-24 12:44:20 +0900

    Add completed script

--- shell/add-codecs.sh
+++ shell/add-codecs.sh
@@ -3,10 +3,39 @@
 
 VER=`lsb_release -r`
 
-if [[ "$VER" == *"42.3" ]]; then
-    echo success
+if [[ "$VER" == *"42.2" ]]; then
+    zypper ref
+    zypper up
+
+    cp -Rv /etc/zypp/repos.d /etc/zypp/repos.d.Old
+    sed -i 's/42\.2/42.3/g' /etc/zypp/repos.d/*
+    zypper ref
+    zypper dup
+
+    zypper ar -f http://packman.inode.at/suse/openSUSE_Leap_42.3/ packman
+    zypper ar -f http://opensuse-guide.org/repo/openSUSE_Leap_42.3/ libdvdcss
+    zypper ref
+    zypper in libdvdcss2 ffmpeg lame gstreamer-plugins-libav \
+        gstreamer-plugins-bad gstreamer-plugins-ugly \
+        gstreamer-plugins-ugly-orig-addon vlc vlc-codecs flash-player \
+        flash-player-ppapi libxine2 libxine2-codecs
+    zypper dup --from packman
+
+    reboot
 fi
 
-#sudo zypper up
+if [[ "$VER" == *"42.2" ]]; then
+    zypper ref
+    zypper up
 
-#if $VER
+    zypper ar -f http://packman.inode.at/suse/openSUSE_Leap_42.3/ packman
+    zypper ar -f http://opensuse-guide.org/repo/openSUSE_Leap_42.3/ libdvdcss
+    zypper ref
+    zypper in libdvdcss2 ffmpeg lame gstreamer-plugins-libav \
+        gstreamer-plugins-bad gstreamer-plugins-ugly \
+        gstreamer-plugins-ugly-orig-addon vlc vlc-codecs flash-player \
+        flash-player-ppapi libxine2 libxine2-codecs
+    zypper dup --from packman
+
+    reboot
+fi
