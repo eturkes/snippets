18c923ad6cdfc93187a2c301ce4a1b829c9065f7
Emir Turkes <eturkes@bu.edu>
2019-01-22 17:12:35 -0500

    Relicense as CC0 1.0 Universal
    I don't want to require anyone to attribute me if they want to use
    this stuff. Also remove anything that isn't compliant with the new
    license.

--- shell/launch-docker-git-clone.sh
+++ shell/launch-docker-git-clone.sh
@@ -1,19 +1,5 @@
 #!/bin/sh
 
-# Copyright 2018 Emir Turkes
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 docker run -it --rm -p 8888:8888 oxtr-ko_2017.03:v1.0 /bin/bash -c \
 	"git clone https://github.com/Neuroinflab/PyMICE.git \
 	/home/jovyan/work && source activate oxtr-ko_2017.03 \
