d59d97467f1808911c534b2eced376d71790f2d3
Emir Turkes <eturkes@bu.edu>
2019-02-03 17:12:44 -0500

    Use double quotes when necessary

--- shell/set-up-autossh.sh
+++ shell/set-up-autossh.sh
@@ -8,5 +8,5 @@
 sudo apt install -y openssh-server autossh
 sudo useradd -m -s /sbin/nologin autossh
 sudo -H -u autossh -s bash -c 'ssh-keygen -t ed25519'
-sudo -H -u autossh -s bash -c 'ssh-copy-id $1 -p $2'
-sudo -H -u autossh -s bash -c 'autossh -M 0 autossh@$1 -p $2 -N -R $3:localhost:$2'
+sudo -H -u autossh -s bash -c "ssh-copy-id $1 -p $2"
+sudo -H -u autossh -s bash -c "autossh -M 0 autossh@$1 -p $2 -N -R $3:localhost:$2"
