1928cc648e718fcff0e332edd82e0a80ef7d5c54
Emir Turkes <eturkes@bu.edu>
2019-02-03 15:25:28 -0500

    Use su command option with autossh user

--- shell/set-up-autossh.sh
+++ shell/set-up-autossh.sh
@@ -7,11 +7,7 @@
 # $4 is the host's SSH port
 
 sudo apt install -y autossh
-
 sudo useradd -m -s /sbin/nologin autossh
-sudo su - autossh -s /bin/bash
-
-ssh-keygen -t ed25519
-ssh-copy-id $2 -p $3
-
-autossh -M 0 $1@$2 -p $3 -N -R $4:localhost:$3
+sudo su - autossh bash -c 'ssh-keygen -t ed25519'
+sudo su - autossh bash -c 'ssh-copy-id $2 -p $3'
+sudo su - autossh bash -c 'autossh -M 0 $1@$2 -p $3 -N -R $4:localhost:$3'
