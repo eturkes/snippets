1a055292a29ce757d91d105d598ab8202360648a
Emir Turkes <eturkes@bu.edu>
2019-02-03 22:09:12 -0500

    Make AutoSSH a cron job

--- shell/set-up-autossh.sh
+++ shell/set-up-autossh.sh
@@ -6,10 +6,14 @@
 # $3 is the host's SSH port
 
 sudo apt install -y openssh-server autossh
-sudo sed -i 's/Port\ 22/Port\ 9991/g' /etc/ssh/sshd_config
+
+sudo sed -i "s/Port\ 22/Port\ $2/g" /etc/ssh/sshd_config
 sudo ufw enable
-sudo ufw allow 9991/tcp
+sudo ufw allow $2/tcp
+
 sudo useradd -m -s /sbin/nologin autossh
 sudo -H -u autossh -s bash -c 'ssh-keygen -t ed25519'
 sudo -H -u autossh -s bash -c "ssh-copy-id $1 -p $2"
-sudo -H -u autossh -s bash -c "autossh -M 0 autossh@$1 -p $2 -N -R $3:localhost:$2"
+
+cron_string="@reboot /bin/sudo -u autossh bash -c '/usr/local/bin/autossh -M 0 -f autossh@$1 -p $2 -N -o 'ExitOnForwardFailure=yes' -o 'ServerAliveInterval 60' -o 'ServerAliveCountMax 3' -R $3:localhost:$2'"
+(crontab -l 2>/dev/null; echo "$cron_string") | crontab -
