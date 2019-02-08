39b9eafd81b05db46fc928ea5f24fd8835854193
Emir Turkes <eturkes@bu.edu>
2019-02-08 18:36:17 -0500

    Refactor

--- shell/set-up-autossh.sh
+++ shell/set-up-autossh.sh
@@ -5,15 +5,13 @@
 # $2 is the client's SSH port
 # $3 is the host's SSH port
 
-sudo apt install -y openssh-server autossh
-
-sudo sed -i "s/Port\ 22/Port\ $2/g" /etc/ssh/sshd_config
-sudo ufw enable
-sudo ufw allow $2/tcp
-
-sudo useradd -m -s /sbin/nologin autossh
-sudo -H -u autossh -s bash -c 'ssh-keygen -t ed25519'
-sudo -H -u autossh -s bash -c "ssh-copy-id $1 -p $2"
-
 cron_string="@reboot /usr/bin/sudo -u autossh bash -c '/usr/bin/autossh -M 0 -f autossh@$1 -p $2 -N -o 'ExitOnForwardFailure=yes' -o 'ServerAliveInterval 60' -o 'ServerAliveCountMax 3' -R $3:localhost:$2'"
-sudo bash -c "(crontab -l 2>/dev/null; echo '$cron_string') | crontab -"
+
+sudo apt install -y openssh-server autossh \
+    && sudo sed -i "s/Port\ 22/Port\ $2/g" /etc/ssh/sshd_config \
+    && sudo ufw enable \
+    && sudo ufw allow $2/tcp \
+    && sudo useradd -m -s /sbin/nologin autossh \
+    && sudo -H -u autossh -s bash -c 'ssh-keygen -t ed25519' \
+    && sudo -H -u autossh -s bash -c "ssh-copy-id $1 -p $2" \
+    && sudo bash -c "(crontab -l 2>/dev/null; echo '$cron_string') | crontab -"
